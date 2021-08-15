e6a8b1b5d2809648cfebe8094969c5cfb80c509e
samtalki <saimouer@gmail.com>
2021-08-15 14:42:36 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.17

---
+++ src/SparseSensors.jl
@@ -0,0 +1,5 @@
+module SparseSensors
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SparseSensors
+using Test
+
+@testset "SparseSensors.jl" begin
+    # Write your tests here.
+end
