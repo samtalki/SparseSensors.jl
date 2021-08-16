a7a570c94c79c573c9dfb42cdc01d1de75a55ba6
samtalki <saimouer@gmail.com>
2021-08-16 09:13:19 -0400

    Vandermonde basis, get_sensors()

--- src/SparseSensors.jl
+++ src/SparseSensors.jl
@@ -1,6 +1,26 @@
 module SparseSensors
+using LinearAlgebra: Matrix, conj
 greet() = print("Hello World!")
-using LinearAlgebra,SpecialMatrices
+using LinearAlgebra
 include("qr.jl")
 include("ccqr.jl")
+include("basis.jl")
+include("reconstruct.jl")
+
+export QRPivot,CostQRPivot,fit,VandermondeBasis
+
+export get_sensors
+
+function get_sensors(sampler)
+    return sampler.pivots
 end
+
+function get_sensors(pivots::AbstractArray,n_sensors::Int)
+    return pivots[1:n_sensors]
+end
+
+function get_sensors(sampler,n_sensors)
+    return sampler.pivots[1:n_sensors]
+end
+end
+
--- src/basis.jl
+++ src/basis.jl
@@ -1,25 +1,22 @@
-
-
-struct VanderMondeBasis
-    Ψ::AbstractArray
+struct Basis
+    Ψ::Matrix
     r::Int
 end
 
-struct SVDBasis
-    Ψ::AbstractArray
-    r::Int
-end
-
-function svd_basis(x,r::Int)
-    #To be implemented
-end
-
-function vandermonde_basis(x,r::Int)
+function VandermondeBasis(x,r::Int;verbose::Bool=false)
     """Makes a vandermonde basis given a range and a number of basis modes"""
-    Ψ = zeros(Float64,length(x),r)
+    Ψ = zeros((length(x),r))
     for (j,col) in enumerate(eachcol(Ψ))
+        ψ = make_vander_column(j,x)
+        if verbose
+            println("ψ","_"*string(j)," size: ",size(ψ))
+        end
         Ψ[:,j] = make_vander_column(j,x) #Each col is x^(k-1)
-    return VanderMondeBasis(Ψ,r) #Truncated basis matrix
+    end
+    if verbose
+        print("Final Ψ size: ",size(Ψ))
+    end
+    return Basis(Ψ,r) #Truncated basis matrix
 end
 
 function make_vander_column(k,col_k)
